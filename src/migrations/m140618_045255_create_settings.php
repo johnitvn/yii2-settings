c09555293735f61d3dd414545870888814076f65
John Martin <john.itvn@gmail.com>
2015-06-28 00:45:57 +0700

    Update
    Update

---
+++ src/migrations/m140618_045255_create_settings.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Schema;
+
+
+class m140618_045255_create_settings extends \yii\db\Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'settings',
+            [
+                'id' => Schema::TYPE_PK,
+                'type' => Schema::TYPE_STRING,
+                'section' => Schema::TYPE_STRING,
+                'key' => Schema::TYPE_STRING,
+                'value' => Schema::TYPE_TEXT,
+                'active' => Schema::TYPE_BOOLEAN,
+                'created' => Schema::TYPE_DATETIME,
+                'modified' => Schema::TYPE_DATETIME,
+            ]
+        );
+    }
+
+    public function down()
+    {
+        echo "m140618_045255_create_settings cannot be reverted.\n";
+
+        return false;
+    }
+}
